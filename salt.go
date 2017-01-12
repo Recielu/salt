7f0ac91b80525e62fb6a0d8a7e6a5c96de27ea81
shirase_aoi <ieee0824@ezweb.ne.jp>
2017-01-12 23:12:48 +0900

    Implementation of salt

---
+++ salt.go
@@ -0,0 +1,15 @@
+package salt
+
+import (
+	"encoding/hex"
+
+	"golang.org/x/crypto/scrypt"
+)
+
+func Convert(pass, slat string) (string, error) {
+	converted, err := scrypt.Key([]byte(pass), []byte(salt), 16384, 8, 1, 32)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(converted[:]), nil
+}
