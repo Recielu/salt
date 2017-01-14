be626e9e7fa7d6cdec6ce06250742fe6398678e1
shirase_aoi <ieee0824@ezweb.ne.jp>
2017-01-14 22:56:24 +0900

    fix typo

--- salt.go
+++ salt.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/crypto/scrypt"
 )
 
-func Convert(pass, slat string) (string, error) {
+func Convert(pass, salt string) (string, error) {
 	converted, err := scrypt.Key([]byte(pass), []byte(salt), 16384, 8, 1, 32)
 	if err != nil {
 		return "", err
